879b584ae95194daa5ef3c8da5cb471b0e3237e1
Nakul <agentvasco69@gmail.com>
2025-05-29 04:31:50 +0000

    Reverted to commit 82e474bfb94423f3b379f09dc9038ed87be7a68d

--- src/components/Navigation.tsx
+++ src/components/Navigation.tsx
@@ -57,7 +57,7 @@ const Navigation = () => {
                 <button
                   key={item.href}
                   onClick={() => scrollToSection(item.href)}
-                  className="nav-item-modern group"
+                  className="nav-item-modern"
                   style={{ animationDelay: `${index * 100}ms` }}
                 >
                   <span className="relative z-10">{item.label}</span>
