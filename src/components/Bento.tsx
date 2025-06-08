8061648e654dd5cf2a8d51e8af013df471f5f2e8
Nakul <agentvasco69@gmail.com>
2025-06-08 19:07:01 +0000

    Refactor: Implement new bento grid layout
    Restructured the About Me section's bento grid to match the provided image layout, including a new "About Me" card, adjusted card positions, and removed the Location card. Modified grid column and row configurations for better proportions and control.

--- src/components/Bento.tsx
+++ src/components/Bento.tsx
@@ -1,3 +1,4 @@
+
 import React, { useState } from 'react';
 import { X } from 'lucide-react';
 import { Dialog, DialogContent, DialogTrigger, DialogClose } from './ui/dialog';
@@ -5,45 +6,55 @@ import Monkeytype from './monkeytype'
 import Blender from './Blender'
 import TechStack from './techstack'
 import Socials from './socials'
+import AboutMeCard from './AboutMeCard'
 
 const Bento = () => {
   const [isModalOpen, setIsModalOpen] = useState(false);
 
   return (
     <section id="about" className="px-6 lg:px-8 relative">
-
         {/* Bento Grid */}
         <section id="about-me" className="max-w-7xl mx-auto px-6 py-2 relative">
-          <div className="grid grid-cols-12 gap-6 auto-rows-[140px]">
+          <div className="grid grid-cols-10 gap-6 auto-rows-[100px]">
             
+            {/* Blender Card - Top Left (Large) */}
+            <div className="col-span-4 row-span-3">
+              <Blender />
+            </div>
 
-            {/* Card B - 3D Renders (3x2 rectangle) */}
-            <Blender></Blender>
+            {/* About Me Card - Top Center */}
+            <div className="col-span-3 row-span-2">
+              <AboutMeCard />
+            </div>
 
-            {/* Card C - Typing Stats (2x2 square) */}
-            <Monkeytype></Monkeytype>
+            {/* Monkeytype Card - Top Right */}
+            <div className="col-span-3 row-span-3">
+              <Monkeytype />
+            </div>
 
-            {/* Card C - Location (2x2 square) */}
-
-
-            {/* Card D - Social Icons (4x1 rectangle) */}
-            <Socials></Socials>
-
-            {/* Card E - Spotify Embed (4x1 rectangle) */}
+            {/* Spotify Card - Below About Me */}
             <div className="col-span-4 row-span-2">
+              <iframe
+                src="https://open.spotify.com/embed/track/0GONea6G2XdnHWjNZd6zt3?utm_source=generator&theme=0"
+                className="w-full h-full rounded-2xl"
+                allowFullScreen
+                allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture"
+                loading="lazy"
+              />
+            </div>
 
-                <iframe
-                  src="https://open.spotify.com/embed/track/0GONea6G2XdnHWjNZd6zt3?utm_source=generator&theme=0"
-                  className="w-[100%] h-[100%] rounded-2xl"
-                  allowFullScreen
-                  allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture"
-                  loading="lazy"
-                />
+            {/* Socials Card - Right of Spotify, Below Monkeytype */}
+            <div className="col-span-3 row-span-1">
+              <Socials />
+            </div>
 
-            </div><br />
+            {/* Empty space row for spacing */}
+            <div className="col-span-full row-span-1"></div>
 
-            {/* Card F - Tech Stack Marquee (4x1 rectangle) */}
-            <TechStack></TechStack>
+            {/* Tech Stack Card - Full Width Bottom */}
+            <div className="col-span-full row-span-1">
+              <TechStack />
+            </div>
 
           </div>
         </section>
