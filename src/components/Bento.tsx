ebe38ec01a661a62253ac1c267df325452664c79
Nakul <agentvasco69@gmail.com>
2025-06-09 01:00:25 +0530

    Revert "Refactor Bento grid layout"
    This reverts commit 578e4f0850d3662866ec33876817be925f4ab607.

--- src/components/Bento.tsx
+++ src/components/Bento.tsx
@@ -15,43 +15,36 @@ const Bento = () => {
     <section id="about" className="px-6 lg:px-8 relative">
         {/* Bento Grid */}
         <section id="about-me" className="max-w-7xl mx-auto px-6 py-2 relative">
-          <div className="grid grid-cols-16 gap-6 auto-rows-[80px]">
+          <div className="grid grid-cols-10 gap-6 auto-rows-[100px]">
             
-            {/* Blender Card - Top Left (Narrower but Longer) */}
-            <div className="col-span-4 row-span-6">
+            {/* Blender Card - Top Left (Large) */}
+            <div className="col-span-4 row-span-3">
               <Blender />
             </div>
 
             {/* About Me Card - Top Center */}
-            <div className="col-span-6 row-span-2">
+            <div className="col-span-3 row-span-2">
               <AboutMeCard />
             </div>
 
-            {/* Monkeytype Card - Top Right (Longer than About Me) */}
-            <div className="col-span-6 row-span-3">
+            {/* Monkeytype Card - Top Right */}
+            <div className="col-span-3 row-span-3">
               <Monkeytype />
             </div>
 
-            {/* Spotify Card - Beside Blender, Below About Me */}
-            <div className="col-span-6 row-span-4">
-              <div className="w-full h-full bg-black/30 border border-white/20 rounded-2xl 
-              shadow-lg transition transform duration-300 hover:scale-105 hover:border-white/40 
-              hover:drop-shadow-[0_0_10px_rgba(100,200,255,0.25)] p-4 flex items-center justify-center">
-                <iframe 
-                  style={{borderRadius: '12px'}} 
-                  src="https://open.spotify.com/embed/track/0GONea6G2XdnHWjNZd6zt3?utm_source=generator" 
-                  width="100%" 
-                  height="100%" 
-                  frameBorder="0" 
-                  allowFullScreen 
-                  allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture" 
-                  loading="lazy"
-                />
-              </div>
+            {/* Spotify Card - Below About Me */}
+            <div className="col-span-4 row-span-2">
+              <iframe
+                src="https://open.spotify.com/embed/track/0GONea6G2XdnHWjNZd6zt3?utm_source=generator&theme=0"
+                className="w-full h-full rounded-2xl"
+                allowFullScreen
+                allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture"
+                loading="lazy"
+              />
             </div>
 
             {/* Socials Card - Right of Spotify, Below Monkeytype */}
-            <div className="col-span-6 row-span-1">
+            <div className="col-span-3 row-span-1">
               <Socials />
             </div>
 
