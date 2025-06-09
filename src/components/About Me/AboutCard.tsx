855cb21f7714d9c06bb1f47f78a78612abd34c62
Nakul <agentvasco69@gmail.com>
2025-06-09 23:45:56 +0530

    responsive bento

--- src/components/About Me/AboutCard.tsx
+++ src/components/About Me/AboutCard.tsx
@@ -2,14 +2,14 @@ import React from 'react';
 
 const AboutCard = () => {
   return (
-    <div className="w-full h-full bg-gradient-to-r from-firebase-purple to-firebase-orange rounded-full p-[3px] 
+    <div className="w-full h-full bg-gradient-to-r from-firebase-purple to-firebase-orange rounded-full p-[2px] 
     shadow-lg transition transform duration-300 hover:scale-105 
     hover:shadow-[0_0_20px_rgba(255,95,31,0.1),0_0_40px_rgba(188,19,254,0.1)]">
       
       <div className="w-full h-full bg-black/100 rounded-full p-6 flex flex-col justify-center">
         <p className="text-white/80 text-sm leading-relaxed text-center">
-          I'm a web developer who enjoys crafting cool websites. Outside of coding, 
-          I'm love touch typing and I also explore 3D animation as a hobby.
+          Hello there <br />
+          General Kenobi.... you are a brave one.
         </p>
       </div>
     </div>
--- src/components/About Me/Bento.tsx
+++ src/components/About Me/Bento.tsx
@@ -1,12 +1,11 @@
-
 import React, { useState } from 'react';
 import { X } from 'lucide-react';
 import { Dialog, DialogContent, DialogTrigger, DialogClose } from '@/blocks/ui/dialog';
-import Monkeytype from './monkeytype'
-import Blender from './Blender'
-import TechStack from './techstack'
-import Socials from './socials'
-import AboutMeCard from './AboutCard'
+import Monkeytype from './monkeytype';
+import Blender from './Blender';
+import TechStack from './techstack';
+import Socials from './socials';
+import AboutMeCard from './AboutCard';
 import Spotify from './Spotify';
 
 const Bento = () => {
@@ -14,44 +13,80 @@ const Bento = () => {
 
   return (
     <section id="about" className="px-6 lg:px-8 relative">
-        {/* Bento Grid */}
-        <section id="about-me" className="max-w-7xl mx-auto px-6 py-2 relative">
-          <div className="grid grid-cols-12 gap-5 auto-rows-[100px]">
-            
-            {/* Blender Card - Top Left (Large) */}
-            <div className="col-start-1 col-span-4 row-span-4 ">
-              <Blender />
-            </div>
-
-            {/* About Me Card - Top Center */}
-            <div className="col-start-5 col-span-4 row-span-2">
-              <AboutMeCard />
-            </div>
-
-            {/* Monkeytype Card - Top Right */}
-            <div className="col-start-9 col-span-4 row-span-3">
-              <Monkeytype />
-            </div>
-
-            {/* Spotify Card - Below About Me */}
-            <div className="col-start-5 col-span-4 row-span-2">
-              <Spotify></Spotify>
-            </div>
-
-            {/* Socials Card - Right of Spotify, Below Monkeytype */}
-            <div className="col-start-9 row-start-4 col-span-4 row-span-1">
-              <Socials />
-            </div>
-
-            {/* Empty space row for spacing */}
-
-            {/* Tech Stack Card - Full Width Bottom */}
-            <div className="col-span-full row-span-1">
-              <TechStack />
-            </div>
-
+      <section id="about-me" className="max-w-7xl mx-auto px-6 py-4 relative">
+        <div
+          className="
+            grid
+            grid-cols-1 
+            sm:grid-cols-2 
+            md:grid-cols-3 
+            lg:grid-cols-12
+            gap-5
+            auto-rows-auto
+            md:auto-rows-[100px]
+          "
+        >
+          {/* Blender Card */}
+          <div className="
+              col-span-1 
+              sm:col-span-2 
+              md:col-span-3 
+              lg:col-start-1 lg:col-span-4 
+              row-span-4
+            ">
+            <Blender />
           </div>
-        </section>
+
+          {/* About Me Card */}
+          <div className="
+              col-span-1 
+              sm:col-span-2 
+              md:col-span-3 
+              lg:col-start-5 lg:col-span-4 
+              row-span-2
+            ">
+            <AboutMeCard />
+          </div>
+
+          {/* Monkeytype Card */}
+          <div className="
+              col-span-1 
+              sm:col-span-2 
+              md:col-span-3 
+              lg:col-start-9 lg:col-span-4 
+              row-span-3
+            ">
+            <Monkeytype />
+          </div>
+
+          {/* Spotify Card */}
+          <div className="
+              col-span-1 
+              sm:col-span-2 
+              md:col-span-3 
+              lg:col-start-5 lg:col-span-4 
+              row-span-2
+            ">
+            <Spotify />
+          </div>
+
+          {/* Socials Card */}
+          <div className="
+              col-span-1 
+              sm:col-span-2 
+              md:col-span-3 
+              lg:col-start-9 lg:col-span-4 
+              row-span-1
+            ">
+            <Socials />
+          </div>
+
+          {/* Tech Stack Card */}
+          <div className="col-span-1 sm:col-span-2 md:col-span-3 lg:col-span-full row-span-1">
+            <TechStack />
+          </div>
+        </div>
+      </section>
     </section>
   );
 };
